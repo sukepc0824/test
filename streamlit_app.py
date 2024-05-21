4af5a151cc4457f779d6f20574d7d6822f8360ab
pcsuke <110383951+sukepc0824@users.noreply.github.com>
2024-05-21 09:36:14 +0000

    a
    Co-authored-by: kouki0404 <kouki0404@users.noreply.github.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,28 @@
 import streamlit as st
+add_selectbox = st.sidebar.selectbox(
+    "何をプレイする？",
+    ("因数分解", "公式クイズ", )
+)
 
-st.title("BMI計算")
-w = st.number_input("体重(kg)")
-h = st.number_input("身長(cm)")
+st.title("因数分解")
+st.text("好きな数字を選んでください")
 
-if st.button("計算"):
-    st.write("あなたのBMIは "+str( (w/(h/100)**2)))
+number = st.slider("スライダー",min_value=0, max_value=100)
+
+st.button("ここをタップ")
+if st.button("ここをタップ"):
+    st.write(number + number % 6 + number / 3)
+else:
+    st.write("数字を選んでください")
+""" 
+https://docs.streamlit.io/develop/api-reference
+↑困ったらこれを見よう！！
+
+//アイデア
+全部作ろう！！！！
+・因数分解（割と簡単）
+    ランダムな数a,b,c,dを生成
+    2*a+3*b+5*c+7*d=素数ではない数を生成
+    
+・公式クイズ（割と複雑）←おみくじ
+"""
